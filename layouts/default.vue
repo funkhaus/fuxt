f775b4b6b1980883b6dac6e57c9cd14477d1190d
Luke Waldner <luke@funkhaus.us>
2019-04-04 16:21:18 -0700

    set up nuxt app with apollo plugin talking to wordpress through graphql

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <div v-for="post in posts.edges">
+      {{ posts }}
+    </div>
+  </section>
+</template>
+
+<script>
+
+import gql from 'graphql-tag'
+import postQuery from '~/queries/Post.gql'
+
+export default {
+    data() {
+        return {
+            posts: ""
+        }
+    },
+    apollo: {
+        posts: {
+            query: postQuery,
+            variables: {
+              first: 1,
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+
+</style>
