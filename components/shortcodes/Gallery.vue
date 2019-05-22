45ef2d0966dd9d2720462db2c13695359abbdfd5
Drew Baker <drewrb@gmail.com>
2019-05-21 17:31:04 -0700

    Added gallery shortcode and updated image fragments

---
+++ components/shortcodes/Gallery.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="shortcode-gallery">
+        <!-- <responsive-image
+            v-for="(image, i) in images"
+            :key="i"
+            :image="image"
+            :aspect-ratio="56.25"
+            class="gallery-block"
+        /> -->
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        images: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "~/styles/_vars.scss";
+.shortcode-gallery {
+    font-size: 0;
+    letter-spacing: 0;
+
+    .gallery-block {
+        width: 33.33%;
+        display: inline-block;
+        padding: 0 5px;
+        margin-bottom: 10px;
+        box-sizing: border-box;
+    }
+}
+</style>
