4ec19dc7b557ef7e17cff51eb632e2cbf4d13b07
derek chiampas <derek.chiampas@gmail.com>
2019-07-23 12:04:22 -0700

    adds scscs vars globally via style-resources package in nuxt.config; removes import from pages and comonents

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,89 +1,96 @@
-import pkg from "./package";
+import pkg from "./package"
 
 export default {
-  mode: "universal",
+    mode: "universal",
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: "utf-8" },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      }
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     ** See: https://nuxtjs.org/api/configuration-loading#use-a-custom-loading-component
+     */
+    loading: {},
+    /*
+     ** Global CSS
+     */
+    css: [
+        "~/styles/base.scss",
+        "~/styles/transitions.scss"
+        //'~/styles/fonts.css'
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
-  },
 
-  /*
-   ** Customize the progress-bar color
-   ** See: https://nuxtjs.org/api/configuration-loading#use-a-custom-loading-component
-   */
-  loading: {},
-  /*
-   ** Global CSS
-   */
-  css: [
-    "~/styles/base.scss",
-    "~/styles/transitions.scss"
-    //'~/styles/fonts.css'
-  ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        { src: "~/plugins/global-component-loader.js" },
+        { src: "~/plugins/global-svg-loader.js" },
+        { src: "~/plugins/google-gtag.client.js", mode: "client" }
+        //{ src: "~/plugins/web-font-loader.client.js", mode: "client" }
+    ],
 
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    { src: "~/plugins/global-component-loader.js" },
-    { src: "~/plugins/global-svg-loader.js" },
-    { src: "~/plugins/google-gtag.client.js", mode: "client" }
-    //{ src: "~/plugins/web-font-loader.client.js", mode: "client" }
-  ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: ["@nuxtjs/apollo", "@nuxtjs/style-resources"],
 
-  /*
-   ** Nuxt.js modules
-   */
-  modules: ["@nuxtjs/apollo"],
+    /*
+     ** Load scss globally via styleResources
+     */
+    styleResources: {
+        scss: ["~/styles/_vars.scss"]
+    },
 
-  /*
-   ** Apollo options. Used for Graph QL queries
-   ** See: https://www.apollographql.com/docs/link/links/http.html#options
-   */
-  apollo: {
-    clientConfigs: {
-      default: {
-        httpEndpoint: "http://stackhaus-backend.local/graphql",
-        tokenName: "apollo-token",
-        persisting: false,
-        websocketsOnly: false
-      }
+    /*
+     ** Apollo options. Used for Graph QL queries
+     ** See: https://www.apollographql.com/docs/link/links/http.html#options
+     */
+    apollo: {
+        clientConfigs: {
+            default: {
+                httpEndpoint: "http://stackhaus-backend.local/graphql",
+                tokenName: "apollo-token",
+                persisting: false,
+                websocketsOnly: false
+            }
+        }
+    },
+
+    /*
+     ** Customize router
+     */
+    router: {
+        linkExactActiveClass: "exact-active-link",
+        linkActiveClass: "active-link"
+        // Example to override router functionality
+        //     extendRoutes (routes, resolve) {
+        //     routes.push({
+        //       name: '404',
+        //       path: '*',
+        //       component: resolve(__dirname, 'pages/404.vue')
+        //     })
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        extend(config, ctx) {
+            // This is used by plugins/global-svg-loader.js
+            config.module.noParse = /\/assets\/svgs\/.+(svg$)/
+        }
     }
-  },
-
-  /*
-   ** Customize router
-   */
-  router: {
-    linkExactActiveClass: "exact-active-link",
-    linkActiveClass: "active-link"
-    // Example to override router functionality
-    //     extendRoutes (routes, resolve) {
-    //     routes.push({
-    //       name: '404',
-    //       path: '*',
-    //       component: resolve(__dirname, 'pages/404.vue')
-    //     })
-  },
-
-  /*
-   ** Build configuration
-   */
-  build: {
-    extend(config, ctx) {
-      // This is used by plugins/global-svg-loader.js
-      config.module.noParse = /\/assets\/svgs\/.+(svg$)/;
-    }
-  }
-};
+}
