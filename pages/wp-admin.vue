1ad435589f1272727d2c859f68dd66b3953fafe1
dChiamp <derek.chiampas@gmail.com>
2019-09-17 17:30:09 -0700

    adds wp-admin redirect from frontend
    adds page to redirect from frontend/wp-admin to backed/wp-admin

---
+++ pages/wp-admin.vue
@@ -0,0 +1,27 @@
+<template>
+    <site-loading />
+</template>
+
+<script>
+import _get from "lodash/get"
+import siteLoading from "~/components/site/Loading"
+import config from "~/nuxt.config"
+
+export default {
+    components: {
+        siteLoading
+    },
+    transition: "fade",
+    mounted() {
+        let gqlEndpoint = _get(
+            config,
+            "apollo.clientConfigs.default.httpEndpoint",
+            ""
+        )
+
+        let wpAdmin = `${gqlEndpoint.split("/graphql")[0]}/wp-admin`
+
+        window.location = wpAdmin
+    }
+}
+</script>
