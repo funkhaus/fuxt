ff292f4f6a4bdf937cc6c09380ccb4f81baa5caf
Luke Waldner <luke@funkhaus.us>
2019-10-16 15:26:22 -0700

    Added shortcode dir to load components from

--- plugins/global-component-loader.js
+++ plugins/global-component-loader.js
@@ -1,24 +1,22 @@
 import Vue from "vue"
 import _kebabCase from "lodash/kebabCase"
 
-// Register all files inside the root of ~/components globally
-const components = require.context("~/components", false, /[A-Z]\w+\.(vue)$/)
-components.keys().map(fileName => {
-    // Get component config
-    const componentConfig = components(fileName)
+// Register all files inside the root of ~/components and ~/components/shortcode
+let componentDirs = [
+    require.context("~/components", false, /[A-Z]\w+\.(vue)$/),
+    require.context("~/components/shortcode", false, /[A-Z]\w+\.(vue)$/)
+]
+componentDirs.forEach(components => {
+    components.keys().map(fileName => {
+        // Get component config
+        const componentConfig = components(fileName)
 
-    // Turn './ComponentName.vue' into 'component-name'
-    const componentName = _kebabCase(
-        fileName.replace(/^\.\//, "").replace(/\.vue$/, "")
-    )
+        // Turn './ComponentName.vue' into 'component-name'
+        const componentName = _kebabCase(
+            fileName.replace(/^\.\//, "").replace(/\.vue$/, "")
+        )
 
-    // Register new component globally
-    Vue.component(componentName, componentConfig.default || componentConfig)
+        // Register new component globally
+        Vue.component(componentName, componentConfig.default || componentConfig)
+    })
 })
-
-// Components used globally. Generally anything that is a shortcode would go here.
-//import shortcodeGallery from "~/components/shortcode/Gallery"
-//Vue.component("shortcode-gallery", shortcodeGallery)
-
-//import shortcodeColumns from "~/components/shortcode/Columns"
-//Vue.component("shortcode-columns", shortcodeColumns)
