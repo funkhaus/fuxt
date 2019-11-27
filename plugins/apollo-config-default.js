2027c3ab80c22d213a127e7b5c984f8b9827fe01
Drew Baker <drewrb@gmail.com>
2019-11-27 00:35:25 -0800

    Update apollo-config-default.js

--- plugins/apollo-config-default.js
+++ plugins/apollo-config-default.js
@@ -1,7 +1,7 @@
 export default function() {
     return {
         httpEndpoint: process.env.DEFAULT_ENDPOINT,
-        getAuth: () => process.env.BASIC_API_TOKEN,
+        getAuth: () => process.env.BASIC_API_TOKEN || "",
         httpLinkOptions: {
             credentials: "include"
         }
