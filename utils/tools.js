04d7e623ca4032e24365f5cb4f038480c256eaa1
Drew Baker <drewrb@gmail.com>
2020-03-26 17:59:41 -0700

    Update tools.js

--- utils/tools.js
+++ utils/tools.js
@@ -1,5 +1,5 @@
-import { _get, _isUndefined } from "lodash"
-
+import _get from "lodash/get"
+import _isUndefined from "lodash/isUndefined"
 
 /*
  * This function builds out a Date using regaulr JS (not moment.js)
@@ -151,15 +151,16 @@ export const initSwipeEvents = (el, deltaMin = 80) => {
         directionEvents = []
     })
 }
+
+
 /*
  * Allows _get() to try multple paths, then return default
  */
-export const _getAll = (obj, paths, defaultValue) => {
+export const _getOne = (obj, paths, defaultValue) => {
     const values = paths
         .map(path => _get(obj, path))
         .filter(v => !_isUndefined(v))
 
-    return values.length ? values : [defaultValue]
+    return values.length ? values[0] : defaultValue
 }
-
 
