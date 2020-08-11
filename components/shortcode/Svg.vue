81348677a691f700ce15096e524066459a8c7514
Drew Baker <drewrb@gmail.com>
2020-08-12 01:59:44 +0200

    Built out shortcode-svg to load a SVG from server

---
+++ components/shortcode/Svg.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props: {
+        url: {
+            type: String,
+            default: ""
+        }
+    },
+    data() {
+        return {
+            svgData: ""
+        }
+    },
+    computed: {
+        parsedSvg() {
+            // TODO Run through SVGO, maybe add a class for styling, preserve viewBox
+            return this.svgData || ""
+        }
+    },
+    async fetch() {
+        const response = await fetch(this.url)
+        this.svgData = await response.text()
+    },
+    render(h) {
+        return h({
+            template: this.parsedSvg
+        })
+    }
+}
+</script>
