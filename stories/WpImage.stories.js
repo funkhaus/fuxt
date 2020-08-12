8e71c7f102abf3a9636471bdec89f27a2ae9d05f
Drew Baker <drewrb@gmail.com>
2020-08-13 00:58:56 +0200

    Update WpImage.stories.js

--- stories/WpImage.stories.js
+++ stories/WpImage.stories.js
@@ -2,25 +2,25 @@ import WpImage from "@/components/WpImage"
 import API from "@/static/db.json"
 
 export default {
-    title: "WpComponents / WpImage",
+    title: "WpComponents / WpImage"
 }
 
 export const Default = () => ({
     components: { WpImage },
     data() {
         return {
-            image: API.image,
+            image: API.image
         }
     },
-    template: `<wp-image :image="image" />`,
+    template: `<wp-image :image="image" />`
 })
 
 export const ImageWithVideo = () => ({
     components: { WpImage },
     data() {
         return {
-            image: API.images[1],
+            image: API.images[1]
         }
     },
-    template: `<wp-image :image="image" />`,
+    template: `<wp-image :image="image" />`
 })
