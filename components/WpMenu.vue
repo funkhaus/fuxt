8bfdb796ce86a85ce3261156927fb3e344dcc6ec
Drew Baker <drewrb@gmail.com>
2020-09-07 16:54:49 +0200

    Gave more flexibility of wp-menu locations

--- components/WpMenu.vue
+++ components/WpMenu.vue
@@ -1,5 +1,8 @@
 <template>
-    <ul :class="classes">
+    <ul
+        :key="location"
+        :class="classes"
+    >
         <slot name="before" />
 
         <wp-menu-item
@@ -41,13 +44,22 @@ export default {
             return
         }
 
-        // Make location name upper case with undersocres instead of spaces
-        const locationName = _upperCase(this.location).replace(/ /g, "_")
+        // WordPress expects menu locations to be SNAKE_CASE.
+        // So if an uppercase location name is given, we use that
+        // so a use can manually do `A24_MENU` which would otherwise
+        // Be converted to `A_24_MENU` if set as  "a24 - Menu"
+        let locationName = this.location
+        if (locationName !== this.location.toUpperCase()) {
+            locationName = _upperCase(this.location).replace(/ /g, "_")
+        }
+
         try {
             const data = await this.$graphql.request(MENU_BY_LOCATION, {
                 location: locationName,
             })
             this.menuItems = _get(data, "menuItems.nodes", [])
+            this.hasLoaded = true
+            this.$emit("loaded")
         } catch (error) {
             console.log("Fetch error in <wp-menu>: ", error)
         }
@@ -55,6 +67,7 @@ export default {
     data() {
         return {
             menuItems: this.items,
+            hasLoaded: false,
         }
     },
     computed: {
@@ -62,6 +75,7 @@ export default {
             return [
                 "wp-menu",
                 `location-${_kebabCase(this.location) || "unknown"}`,
+                { "has-loaded": this.hasLoaded },
             ]
         },
     },
