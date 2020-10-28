023e6af9ee9aab5bf5a76edbb3f0fa0f204b23f8
Drew Baker <drewrb@gmail.com>
2020-10-28 16:04:25 +0100

    Update redirect-trailing-slash.server.js

--- middleware/redirect-trailing-slash.server.js
+++ middleware/redirect-trailing-slash.server.js
@@ -1,21 +1,28 @@
 /*
+ * NOTE This is serverMiddlewear
  * This server middleware makes sure that all URL's end with a slash
  */
-export default function(req, res, next) {
+export default function (req, res, next) {
+    // Simple way of handling query params
     const parts = req.url.split("?")
-    const pathEndsWithSlash = parts[0].endsWith("/")
     const queryParams = parts[1] || ""
+    const pathEndsWithSlash = parts[0].endsWith("/")
 
-    // Handle the redirect, account for ?foo=bar query parameters
-    if (!pathEndsWithSlash) {
-        let redirectUrl = `${parts[0]}/`
-        if (queryParams) {
-            redirectUrl = `${parts[0]}/?${queryParams}`
-        }
+    switch (true) {
+        case req.url.includes("sitemap.xml"):
+            next()
+            break
 
-        res.writeHead(301, { Location: redirectUrl })
-        res.end()
-        return
+        case !pathEndsWithSlash:
+            let redirectUrl = `${parts[0]}/`
+            if (queryParams) {
+                redirectUrl = `${parts[0]}/?${queryParams}`
+            }
+
+            res.writeHead(301, { Location: redirectUrl })
+            return res.end()
+            break
     }
+
     next()
 }
