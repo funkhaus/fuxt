625cdb9323ee43f88c1a1fb9b07eaaaf0c72d08a
dchiamp <derek.chiampas@gmail.com>
2021-04-19 14:05:32 -0700

    udpates gutenberg blocks to use color codes

--- components/gutenberg/Heading.vue
+++ components/gutenberg/Heading.vue
@@ -1,66 +1,66 @@
 <template lang="html">
-    <component
-        :is="element"
-        :id="wpId"
-        :class="classes"
-        :style="styles"
-        v-html="content"
-    />
+  <component
+    :is="element"
+    :id="wpId"
+    :class="classes"
+    :style="styles"
+    v-html="content"
+  />
 </template>
 
 <script>
 export default {
-    props: {
-        level: {
-            type: Number,
-            default: 2,
-        },
-        content: {
-            type: String,
-            default: "",
-        },
-        textColor: {
-            type: String,
-            default: "",
-        },
-        textAlign: {
-            type: String,
-            default: "",
-        },
-        backgroundColor: {
-            type: String,
-            default: "",
-        },
-        fontSize: {
-            type: String,
-            default: "",
-        },
-        wpId: {
-            type: String,
-            default: "",
-        },
-        wpClasses: {
-            type: String,
-            default: "",
-        },
+  props: {
+    level: {
+      type: Number,
+      default: 2
     },
-    computed: {
-        element() {
-            return `h${this.level}`
-        },
-        styles() {
-            return {
-                color: this.textColor,
-                "text-align": this.textAlign,
-                "background-color": this.backgroundColor,
-                "font-size": this.fontSize,
-            }
-        },
-        classes() {
-            return ["gutenberg-heading", `is-h${this.level}`, this.wpClasses]
-        },
+    content: {
+      type: String,
+      default: ""
     },
-}
+    textColorCode: {
+      type: String,
+      default: ""
+    },
+    textAlign: {
+      type: String,
+      default: ""
+    },
+    backgroundColorCode: {
+      type: String,
+      default: ""
+    },
+    fontSize: {
+      type: String,
+      default: ""
+    },
+    wpId: {
+      type: String,
+      default: ""
+    },
+    wpClasses: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    element() {
+      return `h${this.level}`;
+    },
+    styles() {
+      return {
+        color: this.textColorCode,
+        "text-align": this.textAlign,
+        "background-color": this.backgroundColorCode,
+        "font-size": this.fontSize
+      };
+    },
+    classes() {
+      return ["gutenberg-heading", `is-h${this.level}`, this.wpClasses];
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- components/gutenberg/Paragraph.vue
+++ components/gutenberg/Paragraph.vue
@@ -1,58 +1,53 @@
 <template lang="html">
-    <p
-        :id="wpId"
-        :class="classes"
-        :style="styles"
-        v-html="content"
-    />
+  <p :id="wpId" :class="classes" :style="styles" v-html="content" />
 </template>
 
 <script>
 export default {
-    props: {
-        content: {
-            type: String,
-            default: "",
-        },
-        align: {
-            type: String,
-            default: "",
-        },
-        fontSize: {
-            type: String,
-            default: "",
-        },
-        textColor: {
-            type: String,
-            default: "",
-        },
-        backgroundColor: {
-            type: String,
-            default: "",
-        },
-        wpId: {
-            type: String,
-            default: "",
-        },
-        wpClasses: {
-            type: String,
-            default: "",
-        },
+  props: {
+    content: {
+      type: String,
+      default: ""
     },
-    computed: {
-        classes() {
-            return ["gutenberg-paragraph", this.wpClasses]
-        },
-        styles() {
-            return {
-                color: this.textColor,
-                "text-align": this.align,
-                "background-color": this.backgroundColor,
-                "font-size": this.fontSize,
-            }
-        },
+    align: {
+      type: String,
+      default: ""
     },
-}
+    fontSize: {
+      type: String,
+      default: ""
+    },
+    textColorCode: {
+      type: String,
+      default: ""
+    },
+    backgroundColorCode: {
+      type: String,
+      default: ""
+    },
+    wpId: {
+      type: String,
+      default: ""
+    },
+    wpClasses: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    classes() {
+      return ["gutenberg-paragraph", this.wpClasses];
+    },
+    styles() {
+      return {
+        color: this.textColorCode,
+        "text-align": this.align,
+        "background-color": this.backgroundColorCode,
+        "font-size": this.fontSize
+      };
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
