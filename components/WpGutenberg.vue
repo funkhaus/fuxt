3dacb73a52485ccc24f4ebc370980a1c413060c2
dchiamp <derek.chiampas@gmail.com>
2021-04-19 17:40:51 -0700

    adds gallery media items

--- components/WpGutenberg.vue
+++ components/WpGutenberg.vue
@@ -1,91 +1,92 @@
 <template lang="html">
-    <div class="wp-gutenberg">
-        <component
-            :is="block.componentName"
-            v-for="(block, i) in parsedBlocks"
-            v-if="componentIsRegistered(block.name)"
-            :key="i"
-            v-bind="block"
-        />
-    </div>
+  <div class="wp-gutenberg">
+    <component
+      :is="block.componentName"
+      v-for="(block, i) in parsedBlocks"
+      v-if="componentIsRegistered(block.name)"
+      :key="i"
+      v-bind="block"
+    />
+  </div>
 </template>
 
 <script>
 // Helpers
-import _get from "lodash/get"
+import _get from "lodash/get";
 
 export default {
-    components: {
-        GutenbergHeading: () => import("~/components/gutenberg/Heading"),
-        GutenbergParagraph: () => import("~/components/gutenberg/Paragraph"),
-        GutenbergImage: () => import("~/components/gutenberg/Image"),
-        GutenbergQuote: () => import("~/components/gutenberg/Quote"),
-        GutenbergList: () => import("~/components/gutenberg/List"),
-        GutenbergEmbed: () => import("~/components/gutenberg/Embed"),
-        GutenbergColumns: () => import("~/components/gutenberg/Columns"),
-        GutenbergColumns: () => import("~/components/gutenberg/Column"),
-        GutenbergFreeform: () => import("~/components/gutenberg/Freeform"),
+  components: {
+    GutenbergHeading: () => import("~/components/gutenberg/Heading"),
+    GutenbergParagraph: () => import("~/components/gutenberg/Paragraph"),
+    GutenbergImage: () => import("~/components/gutenberg/Image"),
+    GutenbergQuote: () => import("~/components/gutenberg/Quote"),
+    GutenbergList: () => import("~/components/gutenberg/List"),
+    GutenbergEmbed: () => import("~/components/gutenberg/Embed"),
+    GutenbergColumns: () => import("~/components/gutenberg/Columns"),
+    GutenbergColumns: () => import("~/components/gutenberg/Column"),
+    GutenbergFreeform: () => import("~/components/gutenberg/Freeform")
+  },
+  props: {
+    blocks: {
+      type: Array,
+      default: []
+    }
+  },
+  computed: {
+    lowerCaseComponents() {
+      // Get all local component names as lowercase
+      let lowerCaseComponents = Object.keys(this.$options.components || {});
+      return lowerCaseComponents.map(str => {
+        return str.toLowerCase();
+      });
     },
-    props: {
-        blocks: {
-            type: Array,
-            default: [],
-        },
+    parsedBlocks() {
+      // This function is used to shape the data coming out of WP-GQL
+      // to better match the basic prop inputs of each component
+
+      return this.blocks.map(obj => {
+        // Start by flatterning the "attributes"
+        let output = {
+          ...obj,
+          ...obj.attributes
+        };
+
+        // Make name fit with Vue component syntax
+        output.componentName = `${this.getBlockName(obj.name)}`;
+
+        // Shape any props as needed
+        switch (output.componentName) {
+          case "gutenberg-image":
+            output.image = _get(output, "mediaItem.nodes[0]", {});
+            break;
+          case "gutenberg-gallery":
+            output.mediaItems = _get(output, "mediaItems.nodes", []);
+            break;
+        }
+
+        return output;
+      });
+    }
+  },
+  methods: {
+    componentIsRegistered(name = "") {
+      // Get requested component name as all lowercase no dashes
+      let componentName = this.getBlockName(name)
+        .toLowerCase()
+        .replace("-", "");
+
+      // Check that component has been registered
+      return this.lowerCaseComponents.includes(componentName);
     },
-    computed: {
-        lowerCaseComponents() {
-            // Get all local component names as lowercase
-            let lowerCaseComponents = Object.keys(
-                this.$options.components || {}
-            )
-            return lowerCaseComponents.map((str) => {
-                return str.toLowerCase()
-            })
-        },
-        parsedBlocks() {
-            // This function is used to shape the data coming out of WP-GQL
-            // to better match the basic prop inputs of each component
-
-            return this.blocks.map((obj) => {
-                // Start by flatterning the "attributes"
-                let output = {
-                    ...obj,
-                    ...obj.attributes,
-                }
-
-                // Make name fit with Vue component syntax
-                output.componentName = `${this.getBlockName(obj.name)}`
-
-                // Shape any props as needed
-                switch (output.componentName) {
-                    case "gutenberg-image":
-                        output.image = _get(output, "mediaItem.nodes[0]", {})
-                        break
-                }
-
-                return output
-            })
-        },
-    },
-    methods: {
-        componentIsRegistered(name = "") {
-            // Get requested component name as all lowercase no dashes
-            let componentName = this.getBlockName(name)
-                .toLowerCase()
-                .replace("-", "")
-
-            // Check that component has been registered
-            return this.lowerCaseComponents.includes(componentName)
-        },
-        getBlockName(name = "") {
-            return `gutenberg-${name.replace("core/", "").toLowerCase()}`
-        },
-    },
-}
+    getBlockName(name = "") {
+      return `gutenberg-${name.replace("core/", "").toLowerCase()}`;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .wp-gutenberg {
-    // TODO Default styles here
+  // TODO Default styles here
 }
 </style>
---
+++ components/gutenberg/Gallery.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="classes">
+    <wp-image
+      v-for="(image, i) in mediaItems"
+      :key="i"
+      :image="image"
+      :aspect-ratio="56.25"
+      class="gallery-block"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: [Number, String],
+      default: 2
+    },
+    mediaItems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    classes() {
+      return ["gutenberg", "gutenberg-gallery", `columns-${this.columns}`];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.gutenberg-gallery {
+  font-size: 0;
+  letter-spacing: 0;
+  margin: 2em 0;
+
+  // Default 2 columns
+  .gallery-block {
+    display: inline-block;
+    padding: 0 1.5px;
+    margin-bottom: 3px;
+    box-sizing: border-box;
+    width: 50%;
+  }
+
+  // Column variants
+  &.columns-1 {
+    .gallery-block {
+      width: 100%;
+    }
+  }
+  &.columns-3 {
+    .gallery-block {
+      width: 33.33%;
+    }
+  }
+
+  // Multiple galleries used next to each another
+  & + .shortcode-gallery {
+    margin-top: -50px;
+  }
+
+  // Breakpoints
+  // @media #{$lt-tablet} {
+  //     &.columns-2,
+  //     &.columns-3 {
+  //         .gallery-block {
+  //             width: 100%;
+  //         }
+  //     }
+  // }
+}
+</style>
