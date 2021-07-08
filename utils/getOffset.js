c5f6fdaff32cfffbeb195669982c910e10640e2c
drewbaker <drew@funkhaus.us>
2021-07-07 18:55:04 -0700

    Create getOffset.js

---
+++ utils/getOffset.js
@@ -0,0 +1,14 @@
+/*
+ * Returns top and left offsets of an element relative to the document
+ */
+function getOffset(el) {
+    const bodyRect = document.body.getBoundingClientRect()
+    const elemRect = el.getBoundingClientRect()
+
+    return {
+        top: elemRect.top - bodyRect.top,
+        left: elemRect.left - bodyRect.left,
+    }
+}
+
+export default getOffset
