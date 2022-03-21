6d243ab9627be1a3c75a46cd65d4d8f311447469
Drew Baker <drewrb@gmail.com>
2022-03-21 15:26:08 +0000

    Updated packages, renamed template example

--- pages/_default.vue
+++
@@ -1,5 +0,0 @@
-<script>
-// This is how you reuse a template
-import PageDefault from "~/templates/page-default"
-export default PageDefault
-</script>
---
+++ pages/how-to-reuse-template.vue
@@ -0,0 +1,5 @@
+<script>
+// This is how you reuse a template
+import Template from "~/templates/page-default.vue"
+export default Template
+</script>
