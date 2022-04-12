cc8097b78b1f3df20454050d0a1612948b57ebbd
Drew Baker <drewrb@gmail.com>
2022-04-12 12:51:33 -0700

    Added smoothscroll util function

---
+++ utils/smoothScroll.js
@@ -0,0 +1,33 @@
+/*
+ * Smooth scroll to a desired pixel, returns a promise on fail or success.
+ */
+var intervalTimer = null
+function smoothScroll({ top, left, behavior, selector }) {
+    // TODO use getCLientBoundingRect to get pixel value for selector
+
+    window.scrollTo({
+        top,
+        left,
+        behavior,
+    })
+
+    const scrollPromise = new Promise((resolve, reject) => {
+        // Check scroll position, and success when window matches desired value
+        clearInterval(intervalTimer)
+        intervalTimer = setInterval(() => {
+            if (window.scrollY == top) {
+                clearInterval(intervalTimer)
+                resolve("completed")
+            }
+        }, 100)
+
+        // Clear timer in case scroll never completes
+        setTimeout(() => {
+            clearInterval(intervalTimer)
+            reject("failed")
+        }, 2000)
+    })
+
+    return scrollPromise
+}
+export default smoothScroll
