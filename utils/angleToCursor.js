91aa72d47c026e015fc32068068a41d3d0576c6d
Drew Baker <drewrb@gmail.com>
2023-03-18 22:55:33 -0700

    Update angleToCursor.js

--- utils/angleToCursor.js
+++ utils/angleToCursor.js
@@ -3,37 +3,41 @@
  * @param {HTMLElement} el - Element that is going to be rotated
  * @param {mousemove} evt - Mouse event data
  * @param {string} orientation - The orientation where zero should be
+ * @return {Number} - The angle in degrees the cursor is to the element
  */
 function getAngleToCursor(el, evt, orientation = "north") {
+    // get normal angle from mouse to element
     const mouseX = evt.clientX
     const mouseY = evt.clientY
-    
     const elRect = el.getBoundingClientRect()
     const elX = elRect.left + elRect.width / 2
     const elY = elRect.top + elRect.height / 2
 
-    const rads = Math.atan2(mouseY-elY, mouseX-elX)
-    let angle = 0
+    let rads = Math.atan2(mouseY-elY, mouseX-elX)
+    const whole = Math.PI * 2
 
     // Change where zero is located
     switch(orientation) {
-        case "west":
-            angle = rads * 180 / Math.PI + 180
-            break;
-
-        case "north":
-            // 1.571 rads is 90degs. 
-            // Formula is: 90° × π/180 = 1.571rad
-            angle = (rads - 1.571) * 180 / Math.PI + 180
-            break;  
+        case "south":
+            rads -= whole / 4
+            break
 
         case "east":
-        case "south":            
-            // Can add these when/if needed
-            break;
+            rads += 0
+            break
+
+        case "north":
+            rads += whole / 4
+            break
+        
+        case "west":            
+            rads += whole / 2
+            break
     }
 
-    return angle
-}
+    // convert rads to range between 0 and 360 (although still in radians)
+    rads = ((rads % whole) + whole) % whole
 
-export default getAngleToCursor
+    // ReConvert rads into degrees
+    return rads * 180 / Math.PI
+}
