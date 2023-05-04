29df6c0f319153ae3612f5bd425aeec59242e3ec
Drew Baker <drewrb@gmail.com>
2023-05-04 01:28:07 -0700

    Populate Vuex when running in SPA mode

--- plugins/preview.client.js
+++ plugins/preview.client.js
@@ -1,16 +1,23 @@
 /*
- * This plugin denables Nuxt Preview mode.
+ * This plugin enables Nuxt Preview mode.
  * It also allows some custom routing if your WP preview URL's are different than the frontend routes.
  * And exmaple of a WordPress preview URL:
  *    https://example.com/news/p/post-slug-here/?preview=true&id=1234&type=post&status=publish&slug=post-slug-here&uri=/news/p/post-slug-here/
  */
-export default function ({ query, enablePreview, redirect, error, $graphql }) {
+export default function (context) {
+    const { query, store, enablePreview, error, $graphql, redirect } = context
+
     // Enable Nuxt Preview mode
     if (query.preview) {
         $graphql.default.setHeaders({ Preview: true })
         enablePreview()
     } else {
         $graphql.default.setHeaders({})
+    }
+
+    // Populate Vuex when running in SPA mode (like for hidden pages or on Preview)
+    if (!store?.state?.siteMeta?.title || false) {
+        store.dispatch("nuxtGenerateInit", context)
     }
 
     // Can handle some redirects here if Preview URLs are different than frontend.
