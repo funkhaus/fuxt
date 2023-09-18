8f4b44caf5918f7348d6061302828081b9607fbe
Drew Baker <drewrb@gmail.com>
2023-09-18 10:48:25 -0700

    Update countdown.js

--- utils/countdown.js
+++ utils/countdown.js
@@ -1,9 +1,10 @@
-/*
+/**
  * This function returns an object of time remaining between start and end
- * Must pass in a date() object for start and end params.
+ * @see https://stackoverflow.com/a/56159578/503546
  *
- * @RETURNS {'years': 1, 'months': 12, 'days': 59, 'hours': 23, 'seconds': 59}
- * @SEE https://stackoverflow.com/a/56159578/503546
+ * @param {date} start A date object for the starting time
+ * @param {date} end A date object for the time to count down to
+ * @returns {Object} An object that looks like this {'years': 1, 'months': 12, 'days': 59, 'hours': 23, 'seconds': 59}
  */
 
 import {
