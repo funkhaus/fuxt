a13b71f40a026c1404ec4b75aeebba5e239812d6
Drew Baker <drewrb@gmail.com>
2024-10-08 12:36:26 -0700

    Added Aspect ratio, caption, srcset, modes to wp-image

--- app/components/wp-image.vue
+++ app/components/wp-image.vue
@@ -3,29 +3,36 @@
         <img
             v-if="src && !disabled"
             ref="imageEl"
-            :src="src"
-            loading="lazy"
             class="media media-image"
+            :src="src"
+            :srcset="srcSet"
+            :sizes="parsedSizes"
+            loading="lazy"
+            :alt="alt"
             @load="setImageLoaded"
         >
 
         <video
             v-if="videoUrl && !disabled"
+            ref="videoEl"
+            class="media media-video"
             :src="videoUrl"
             :loop="props.loop"
             :autoplay="props.autoplay"
             :muted="props.muted"
             playsinline
             disablePictureInPicture="true"
-            class="media media-video"
+            @playing="onPlaying"
         />
 
-        <!-- TODO Aspect ratio and show video on top -->
-        <!-- TODO Add caption -->
+        <figcaption
+            v-if="caption"
+            class="caption"
+            v-text="caption"
+        />
+
         <!-- TODO Background color -->
-        <!-- TODO Focal points color -->
-        <!-- TODO Sizing modes -->
-        <!-- TODO SrcSets and Sizes -->
+        <!-- TODO Focal points and color -->
         <!-- TODO Only load video when coming into view -->
         <!-- TODO Has-loaded of video? -->
     </figure>
@@ -33,6 +40,7 @@
 
 <script setup lang="ts">
 const imageEl = ref(null)
+const videoEl = ref(null)
 const imageLoaded = ref(false)
 
 // Props
@@ -56,23 +64,109 @@ const props = defineProps({
     muted: {
         type: Boolean,
         default: true
+    },
+    aspectRatio: {
+        type: [String, Number],
+        default: 0
+    },
+    mode: {
+        type: String,
+        default: 'intrinsic-ratio'
+    },
+    objectFit: {
+        type: String,
+        default: 'cover'
+    },
+    sizes: {
+        type: String,
+        default: ''
     }
 })
 
+// Refs
+const isPlaying = ref(false)
+
 // Computeds
-const classes = computed(() => ['wp-image', { 'has-loaded': imageLoaded.value }, { 'has-video': hasVideo.value }])
+const classes = computed(() => [
+    'wp-image',
+    { 'has-loaded': imageLoaded.value },
+    { 'has-video': hasVideo.value },
+    `mode-${props.mode}`,
+    `object-fit-${props.objectFit}`,
+    `is-orientation-${orientation.value}`,
+    { 'is-disabled': props.disabled },
+    { 'is-playing': isPlaying.value }
+])
 const src = computed(() => props.image?.src || '')
 const videoUrl = computed(() => props.image?.acf?.videoUrl || '')
 const hasVideo = computed(() => Boolean(videoUrl.value))
+const height = computed(() => props.image?.height || props.image?.meta?.height || 0)
+const width = computed(() => props.image?.width || props.image?.meta?.width || 0)
+const alt = computed(() => props.image?.alt || '')
+const caption = computed(() => props.image?.caption || '')
+const srcSet = computed(() => props.image?.srcset || '')
+const parsedAspectRatio = computed(() => `${width.value} / ${height.value}`)
+const parsedSizes = computed(() => {
+    let output = props.sizes || props.image?.sizes || ''
+
+    // Handle some custom size shortcuts
+    switch (props.sizes) {
+        case 'full':
+        case 'full-screen':
+        case 'fullscreen':
+            output = '(max-width: 850px) 1920px, 100vw'
+            break
+
+        case 'half':
+        case 'half-screen':
+            output = '(max-width: 850px) 100vw, 50vw'
+            break
+
+        case 'third':
+        case 'third-screen':
+            output = '(max-width: 850px) 100vw, 33.33vw'
+            break
+
+        case 'quarter':
+        case 'quarter-screen':
+            output = '(max-width: 850px) 100vw, 25vw'
+            break
+    }
+
+    return output
+})
+const orientation = computed(() => {
+    let output = 'landscape'
+    switch (true) {
+        case height.value > width.value:
+            output = 'portrait'
+            break
+
+        case height.value == width.value:
+            output = 'square'
+            break
+    }
+    return output
+})
+
+const cssVars = ref({
+    aspectRatio: parsedAspectRatio.value
+})
 
 // Actions
 const setImageLoaded = () => {
     imageLoaded.value = true
 }
+const onPlaying = () => {
+    console.log('Playing')
+
+    isPlaying.value = true
+}
 
 // Lifecycle hooks
 onMounted(() => {
     imageLoaded.value = imageEl.value?.complete || false
+    isPlaying.value = !videoEl.value?.paused || false
 })
 </script>
 
@@ -82,8 +176,62 @@ onMounted(() => {
     width: 100%;
 
     .media {
-        max-width: 100%;
-        height: auto;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+        transition: opacity 0.4s ease-in-out;
+        z-index: 10;
+    }
+    .media-video {
+        z-index: 20;
+    }
+    .caption {
+        display: none;
+    }
+
+    /* Modes */
+    &:where(.mode-intrinsic-ratio) {
+        position: relative;
+        aspect-ratio: v-bind('cssVars.aspectRatio');
+    }
+    &:where(.mode-cover) {
+        position: relative;
+    }
+    &.mode-cover {
+        .sizer {
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            position: absolute;
+        }
+    }
+
+    /* Object fit modes */
+    &.object-fit-cover .media {
+        object-fit: cover;
+    }
+    &.object-fit-contain .media {
+        object-fit: contain;
+    }
+
+    /* Loaded & active states */
+    &.has-loaded {
+        .media-image {
+            opacity: 1;
+        }
+    }
+    &.is-playing {
+        .media-image {
+            /* Hide image when video is playing to avoid overlaps */
+            opacity: 0;
+        }
+        .media-video {
+            opacity: 1;
+        }
     }
 }
 </style>
