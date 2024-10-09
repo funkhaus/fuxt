0b36ae9c1413c9de181ac47d451c791f538645a5
Drew Baker <drewrb@gmail.com>
2024-10-09 11:48:02 -0700

    Cleaned up config and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-09-17',
-
-    // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
     future: { compatibilityVersion: 4 },
 
-    // This only ships on dev, it gets stripped in Production
-    devtools: { enabled: true },
-
+    // Build configuration
     nitro: {
-    // https://github.com/danielroe/nuxt-vercel-isr
         routeRules: {
-            // all routes
+            // All routes should be ISR
             '/**': {
                 isr: true
             }
         },
         prerender: {
+            // This helps ensure that all paths end with `/`.
             autoSubfolderIndex: true
         }
     },
-
     vite: {
         optimizeDeps: {
             // Used for v8.3.5 of Storybook. Can remove after update.
@@ -28,7 +23,12 @@ export default defineNuxtConfig({
             include: ['jsdoc-type-pratt-parser']
         }
     },
+    vue: {
+        // Required for @nuxtjs/storybook
+        runtimeCompiler: process.env.STORYBOOK === 'true'
+    },
 
+    // Nuxt app configuration
     app: {
         head: {
             meta: [
@@ -47,67 +47,24 @@ export default defineNuxtConfig({
             ]
         }
     },
+    devtools: {
+        // This only ships on dev, it gets stripped in Production
+        enabled: true
+    },
 
+    // Runtime ENV parsing
     runtimeConfig: {
         public: {
             wordpressApiUrl: process.env.WORDPRESS_API_URL
         }
     },
 
-    // required for @nuxtjs/storybook
-    vue: {
-        runtimeCompiler: process.env.STORYBOOK === 'true'
-    },
-
+    // CSS and fonts
     css: [
+        '~/assets/css/fonts.css',
         '~/assets/css/vars.css',
         '~/assets/css/main.css'
     ],
-
-    modules: [
-        '@nuxt/fonts',
-        '@pinia/nuxt',
-        '@nuxtjs/storybook',
-        '@nuxtjs/sitemap',
-        '@vueuse/nuxt',
-        'nuxt-lodash'
-    ],
-
-    lodash: {
-        prefix: '_',
-        prefixSkip: []
-    },
-
-    // site: {
-    //     url: 'https://nuxt.com',
-    //     name: 'Nuxt Example'
-    // },
-
-    sitemap: {
-        exclude: []
-    },
-
-    fonts: {
-        // families: [
-        //     {
-        //         name: 'CalibreWeb-Light',
-        //         src: '/fonts/CalibreWeb-Light.woff',
-        //         fallbacks: [
-        //             'sans-serif'
-        //         ],
-        //         weights: ['300'],
-        //         styles: ['normal'],
-        //         display: 'swap'
-        //     }
-        // ]
-        // adobe: {
-        //     id: [
-        //         'twu5pcy'
-        //     ]
-        // }
-
-    },
-
     postcss: {
         plugins: {
             '@csstools/postcss-global-data': {
@@ -116,5 +73,32 @@ export default defineNuxtConfig({
             'postcss-nested': {},
             'postcss-custom-media': {}
         }
+    },
+    fonts: {
+        assets: {
+            prefix: '/fonts/'
+        }
+        // adobe: {
+        //     id: [
+        //         '123456'
+        //     ]
+        // }
+    },
+
+    // Modules and configuration
+    modules: [
+        '@nuxt/fonts',
+        '@pinia/nuxt',
+        '@nuxtjs/storybook',
+        '@nuxtjs/sitemap',
+        '@vueuse/nuxt',
+        'nuxt-lodash'
+    ],
+    lodash: {
+        prefix: '_',
+        prefixSkip: []
+    },
+    sitemap: {
+        exclude: ['/wp-admin/']
     }
 })
