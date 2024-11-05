7768304a370826dc3ff92f16b3a75705abb3c7ea
Anthony Fu <github@antfu.me>
2024-11-05 21:45:38 +0800

    fix: setup Prettier with ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,7 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 export default createConfigForNuxt({
     features: {
-        stylistic: {
-            indent: 4,
-            semi: false,
-            commaDangle: 'never'
-        },
+        stylistic: false,
         typescript: true
     }
 })
