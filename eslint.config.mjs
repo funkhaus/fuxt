468f4ac941c0e15e9de9a5a3ba924e82e8c5b78a
Anthony Fu <github@antfu.me>
2024-12-11 18:16:57 +0800

    feat: setup json rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import pluginJsonc from 'eslint-plugin-jsonc'
+import parserJsonc from 'jsonc-eslint-parser'
 
 export default createConfigForNuxt({
     features: {
@@ -11,4 +13,19 @@ export default createConfigForNuxt({
         formatters: true,
         typescript: true
     }
-})
+}).append(
+    {
+        files: ['**/*.json'],
+        plugins: {
+            jsonc: pluginJsonc
+        },
+        languageOptions: {
+            parser: parserJsonc
+        },
+        name: 'jsonc/rules',
+        rules: {
+            ...pluginJsonc.configs['flat/recommended-with-json'].rules,
+            'jsonc/indent': ['error', 4]
+        }
+    }
+)
