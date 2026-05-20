bee6e54903a860e2a451bc6c182c1e9e6e70c513
Luka Vuk <luka.vuk0909@gmail.com>
2026-05-20 13:29:06 +0200

    fix heading text align

--- app/components/wp-block/core-heading.vue
+++ app/components/wp-block/core-heading.vue
@@ -26,7 +26,7 @@ const props = defineProps({
 const classes = computed(() => [
     'core-heading',
     'margin-text',
-    `align-${props.attrs?.align || 'default'}`,
+    `align-${props.attrs?.align || props.attrs?.textAlign || 'default'}`,
     `is-${props.attrs?.tagName}`
 ])
 const styles = computed(() => ({
